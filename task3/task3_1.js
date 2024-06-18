f7900a10e8f1b375d9c37a403305f2d9ff30430b
Olha <olhapelekh21@gmail.com>
2024-06-19 00:52:18 +0300

    completed task3_1

---
+++ task3/task3_1.js
@@ -0,0 +1,22 @@
+const number = 55;
+const str = 'math';
+const bigNumber = Math.pow(2, 53);
+const istrue = true;
+let value = null;
+let temp;
+const uniqueID = Symbol('number');
+let staff = {
+    name: 'Ivan',
+    age: 20,
+    city: 'Kyiv'
+};
+console.log(typeof number); //Number
+console.log(typeof str); //String
+console.log(typeof bigNumber); //BigInt
+console.log(typeof istrue); //Boolean
+console.log(typeof value); //null is object type
+console.log(typeof temp); //undefined
+console.log(typeof uniqueID); //Symbol
+console.log(typeof staff); //object
+
+
