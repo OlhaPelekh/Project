b09bed3616f20176125d44077c61c317c35eb01e
Olha <olhapelekh21@gmail.com>
2024-06-19 19:23:28 +0300

    completed task3_2, task3_3

--- task3/task3_2.js
+++ task3/task3_2.js
@@ -1,1 +1,13 @@
+const info = {
+    name: 'Ivan',
+    age: 20,
+    city: 'Kyiv'
+};
+
+let qualification = 'beginner';
+
+const education = 'master';
+
+console.log(`My name is ${info.name}. I am ${info.age} years old. My city is ${info.city}. I am ${education} and my qualification is ${qualification}.`);
+
 
--- task3/task3_3.js
+++ task3/task3_3.js
@@ -1,1 +1,19 @@
+// 1 спосіб
+const number = 12345;
+let strNumber = number.toString();
+let result1 = strNumber.split('').join(' ');
+console.log(result1);
+
+// 2 спосіб
+function splitNumber(number) {
+    let mass = [];
+    while (number > 0) {
+        mass.push(number % 10);
+        number = Math.floor(number / 10);
+    }
+    mass.reverse();
+    return mass.join(' ');
+}
+let result2 = splitNumber(number);
+console.log(result2); 
 
