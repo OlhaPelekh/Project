e42c84fea92f172ee93ed85abc9641aa9c03525b
Olha <olhapelekh21@gmail.com>
2024-06-21 23:36:15 +0300

    completed task4_1, task4_2

---
+++ task4/task4_2/task4_2.js
@@ -0,0 +1,22 @@
+let number = prompt('Введіть тризначне число:');
+if (isNaN(number)) {
+    alert('Будь ласка, введіть коректне число.');
+} else {
+    let allSame = true;
+    let someSame = false;
+    for (let i = 0; i < number.length - 1; i++) {
+        for (let j = i + 1; j < number.length; j++)
+            if (number[i] === number[j]) {
+                someSame = true;
+            } else {
+                allSame = false;
+            }
+    }
+    if (allSame) {
+        alert('Всі цифри однакові');
+    } else if (someSame) {
+        alert('Cеред цифр є цифри однакові');
+    } else {
+        alert('Цифри різні');
+    }
+}
