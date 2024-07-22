26644b0ba0e7f3f2cd608326b76bfc2b77ad540d
Olha <olhapelekh21@gmail.com>
2024-07-23 00:40:46 +0300

    task11

---
+++ task11/task11_1/task11_1.js
@@ -0,0 +1,37 @@
+// Вивести таблицю Піфагора (10×10), проте цього разу таблиця повинна бути створена динамічно
+
+const mass = [];
+(() => {
+  for (let i = 0; i <= 10; i++) {
+    let row = [];
+    for (let j = 0; j <= 10; j++) {
+      if (i === 0) {
+        row.push(j);
+      } else if (j === 0) {
+        row.push(i);
+      } else {
+        row.push(i * j);
+      }
+    }
+    mass.push(row);
+  }
+})();
+
+console.log(mass);
+
+const table = document.createElement("table");
+for (let i = 0; i < mass.length; i++) {
+  const row = document.createElement("tr");
+  for (let j = 0; j < mass[i].length; j++) {
+    const columnElement = document.createElement("td");
+    columnElement.textContent = mass[i][j] === 0 ? null : mass[i][j];
+    if (i === j&& i !== 0) {
+      columnElement.style.backgroundColor = "blue";
+    }
+    row.appendChild(columnElement);
+  }
+  table.appendChild(row);
+}
+
+const parentElement = document.querySelector("div");
+parentElement.appendChild(table);
