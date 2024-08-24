e9cd38f3ded1a07b3275bf3a8a2434bf8f4bf256
Olha <olhapelekh21@gmail.com>
2024-08-24 23:31:35 +0300

    task18

---
+++ task18/task18_1/task18_1.js
@@ -0,0 +1,31 @@
+// Реалізувати таймер відліку:
+// Початок таймера визначати із змінної
+// Відобразити на сторінці час у форматі 01:25
+// Коли закінчився таймер зупинити його
+
+const divTimer = document.querySelector(".timer");
+let valueTimer;
+
+do {
+   valueTimer = prompt("Введіть секунди", 85);
+} while (isNaN(valueTimer) || valueTimer === null || valueTimer.trim() === "");
+
+function getTime() {
+  let minutes = Math.floor(valueTimer / 60);
+  let seconds = valueTimer % 60;
+
+  minutes = minutes >= 10 ? minutes : `0${minutes}`;
+  seconds = seconds >= 10 ? seconds : `0${seconds}`;
+
+  divTimer.textContent = `${minutes}:${seconds}`;
+
+  if (valueTimer <= 0) {
+    clearInterval(timer);
+    divTimer.textContent = `00:00`;
+  } else {
+    valueTimer--;
+  }
+}
+
+getTime();
+const timer = setInterval(getTime, 1000);
